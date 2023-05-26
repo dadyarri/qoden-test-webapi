ca7cfc529fc62b20be93e7ff33bfab3ce5adec2b
Daniil <dadyarri@gmail.com>
2023-05-26 23:13:56 +0300

    add test for task 2

---
+++ WebApp.Test/Todo2Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WebApp.Test
+{
+    public class Todo2Test : MyTestBase
+    {
+        [Test(Description = "TODO 2: Attempt to login as non-existent user should response with 404")]
+        public async Task AttemptToLoginAsNonExistentUser__ShouldResponseAs404()
+        {
+            // Act
+            var response = await AliceClient.SignInAsync("userthatdoesnotexist@mailinator.com");
+            
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
