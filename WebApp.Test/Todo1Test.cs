5315ea42fbfed8b4cef77be4833157525e0c3288
Daniil <dadyarri@gmail.com>
2023-05-26 22:57:21 +0300

    add test for task 1

---
+++ WebApp.Test/Todo1Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Net.Http.Headers;
+using NUnit.Framework;
+
+namespace WebApp.Test
+{
+    public class Todo1Test: MyTestBase
+    {
+        [Test(Description = "TODO 1: Sign in should generate auth cookie")]
+        public async Task SignInShouldGenerateAuthCookie()
+        {
+            // Act
+            var response = await AliceClient.SignInAsync("alice@mailinator.com");
+            var cookieHeaderFound = response.Headers.TryGetValues(HeaderNames.SetCookie, out var cookies);
+            
+            // Assert
+            cookieHeaderFound.Should().BeTrue();
+            cookies.Should().NotBeEmpty();
+        }
+    }
+}
