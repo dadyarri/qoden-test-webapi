b86172bb584fb195d2b8e16d15b8028e9e5c7217
Daniil <dadyarri@gmail.com>
2023-05-26 14:08:32 +0300

    fix starting application, remove responsing with hello world

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,21 +1,20 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp
 {
     public class Startup
     {
-        // TODO 0: Something broken in ConfigureServices.
-        // Don't focus attention on it right now, you will faced with problem in process of meeting the challenges TODOs
-        // Pay attention to different possible solutions of the problem
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();
             services.AddSingleton<IAccountDatabase, AccountDatabaseStub>();
             services.AddSingleton<IAccountCache, AccountCache>();
         }
@@ -28,8 +27,12 @@ namespace WebApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints((endpoints) =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
