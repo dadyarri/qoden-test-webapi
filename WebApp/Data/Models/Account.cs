4e5ac87066eef0c4e688ab1637f0423efd9d1bb2
Daniil <dadyarri@gmail.com>
2023-05-27 09:48:46 +0300

    add json property names to account model for proper deserialization

--- WebApp/Data/Models/Account.cs
+++ WebApp/Data/Models/Account.cs
@@ -1,11 +1,18 @@
-﻿namespace WebApp.Data.Models
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Data.Models
 {
     public class Account
     {
+        [JsonPropertyName("userName")]
         public string UserName { get; set; }
+        [JsonPropertyName("externalId")]
         public string ExternalId { get; set; }
+        [JsonPropertyName("internalId")]
         public long InternalId { get; set; }
+        [JsonPropertyName("counter")]
         public int Counter { get; set; }
+        [JsonPropertyName("role")]
         public string Role { get; set; }
 
         public Account Clone() => (Account) MemberwiseClone();
