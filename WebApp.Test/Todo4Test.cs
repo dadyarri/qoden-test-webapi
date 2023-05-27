f909f0c560d1c27f97012535ccfb4c442a56bde6
Daniil <dadyarri@gmail.com>
2023-05-27 09:31:46 +0300

    add tests for tasks 4 and 5

---
+++ WebApp.Test/Todo4Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WebApp.Test
+{
+    public class Todo4Test: MyTestBase
+    {
+        [Test(Description = "TODO4: Unauthorized users should get 401 response on requests to /api/account")]
+        public async Task UnauthorizedUsers__ShouldGet401Response__WhenAccountRouteRequested()
+        {
+            // Act
+            var response = await Client.GetAccountAsync();
+            
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+    }
+}
---
+++ WebApp.Test/Todo5Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WebApp.Test
+{
+    public class Todo5Test: MyTestBase
+    {
+        [Test(Description = "TODO5: Endpoint /api/account/{id} should work with user with admin role")]
+        public async Task GetAccountByInternalId__ShouldWork__WhenUserHaveAdminRole()
+        {
+            // Act
+            var response = await AliceClient.GetAccountByIdAsync(1);
+            
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+        
+        [Test(Description = "TODO5: Endpoint /api/account/{id} shouldn't work with user without admin role")]
+        public async Task GetAccountByInternalId__ShouldNotWork__WhenUserDoesNotHaveAdminRole()
+        {
+            // Act
+            var response = await BobClient.GetAccountByIdAsync(1);
+            
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+    }
+}
